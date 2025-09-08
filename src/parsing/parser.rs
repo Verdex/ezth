66cea885e70c865a6485a45a8fd4514a860c16a0
David L. Inman <davidlevere@gmail.com>
2025-09-07 22:19:26 -0400

    parse todos

--- src/parsing/parser.rs
+++ src/parsing/parser.rs
@@ -1,7 +1,52 @@
 
 use crate::data::*;
 
-pub fn parse(input : &str) -> Result<ExprOrDef, (usize, usize)> {
+use super::lexer;
+
+/*
+    def sym = expr
+
+    match sym = ?
+
+    pat sym = ? 
+
+
+    Expr
+    fun (sym_list) => expr
+    sym
+    expr.sym
+    expr |> call_expr
+    expr |index_pattern> call_expr
+    (expr)
+    expr(expr_list)
+
+    Pattern
+    data (exact)
+    _
+    a
+    $a
+    cons(pattern_list)
+    and
+    or
+    [| pattern_list |]
+    {| next_pattern_list |}
+    predicate
+    match_with ?
+
+
+*/
+
+
+// [fn(input) -> intermedia result]
+
+// TODO need different error
+pub fn parse(input : &str) -> Result<(Vec<ExprOrDef>, ?), ()> {
+
+    let output = lexer::lex(input); 
+
+
+
+
 
     todo!()
 }

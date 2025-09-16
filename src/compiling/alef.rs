b37bede47c2940e97f7551691679a4619d901767
David L. Inman <davidlevere@gmail.com>
2025-09-15 21:41:41 -0400

    inc prog alef

--- src/compiling/alef.rs
+++ src/compiling/alef.rs
@@ -1,10 +1,22 @@
 
 // lets, defs, ops, calls
 
+use std::collections::HashMap;
+use std::rc::Rc;
 use an_a_vm::data::*;
+use crate::data::runtime::*;
 
-/*
-pub fn blarg() -> Result<?, ?> {
+pub enum AlefStmt {
 
 }
-*/
+
+pub struct AlefFun {
+    name : Rc<str>,
+    params : Vec<Rc<str>>,
+    stmts : Vec<AlefStmt>,
+}
+
+pub fn compile(input : Vec<AlefFun>) -> Result<(Vec<Fun<Data>>, HashMap<Rc<str>, usize>), Box<dyn std::error::Error>> {
+
+    todo!()
+}

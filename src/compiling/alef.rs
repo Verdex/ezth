4efcf72f1f3b28fa65fed67883508798b3a2acdf
David L. Inman <davidlevere@gmail.com>
2025-09-17 23:33:52 -0400

    inc prog + test

--- src/compiling/alef.rs
+++ src/compiling/alef.rs
@@ -25,12 +25,14 @@ pub struct AlefFun {
     stmts : Vec<AlefStmt>,
 }
 
+#[derive(Debug)]
 pub enum AlefError {
     LocalDoesNotExist{ local: Rc<str>, fun: Rc<str> },
     LocalRedefined{ local: Rc<str>, fun: Rc<str> },
 }
 
 pub fn compile(input : Vec<AlefFun>, op_map : &HashMap<Rc<str>, usize>) -> Result<Vec<Fun<Data>>, AlefError> {
+    // TODO check for duplicate func names
     let map : HashMap<Rc<str>, usize> = input.iter().enumerate().map(|(i, x)| (Rc::clone(&x.name), i)).collect();
     input.into_iter().map(|f| compile_fun(f, &map, op_map)).collect()
 }
@@ -49,8 +51,8 @@ fn compile_fun(f : AlefFun, fun_map : &HashMap<Rc<str>, usize>, op_map : &HashMa
                 match val { 
                     AlefVal::Data(data) => { instrs.push(Op::PushLocal(data)); },
                     AlefVal::Var(v) => {
-                        let local = locals.get(&v).ok_or(AlefError::LocalDoesNotExist{ local: Rc::clone(&v), fun: Rc::clone(&f.name) })?;
-                        instrs.push(Op::Dup(*local));
+                        let local = get_local(&locals, &v, &f.name)?;
+                        instrs.push(Op::Dup(local));
                     },
                     _=> todo!()
                     //AlefVal::FunCall
@@ -72,5 +74,30 @@ pub fn get_local(map : &HashMap<Rc<str>, usize>, local : &Rc<str>, fun_name : &R
     match map.get(local) {
         Some(local) => Ok(*local),
         None => Err(AlefError::LocalDoesNotExist{ local: Rc::clone(local), fun: Rc::clone(fun_name) })
+    }
+}
+
+#[cfg(test)]
+mod test {
+    use super::*;
+    use an_a_vm::*;
+
+    #[test]
+    fn should_assign_let_from_let_from_value() {
+        let f = AlefFun{
+            name: "fun".into(),
+            params: vec![],
+            stmts: vec![
+                AlefStmt::Let { var: "a".into(), val: AlefVal::Data(19.0) },
+                AlefStmt::Let { var: "b".into(), val: AlefVal::Var("a".into()) },
+                AlefStmt::ReturnVar("b".into()),
+            ],
+        };
+        let fs = compile(vec![f], &HashMap::new()).unwrap();
+        let mut vm : Vm<Data, ()> = Vm::new(fs, vec![]);
+
+        let result = vm.run(0).unwrap().unwrap();
+
+        assert_eq!(result, 19.0);
     }
 }

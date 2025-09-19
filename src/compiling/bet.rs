7d0082bbeabf853fd7d8d05c4f352c02df19595b
David L. Inman <davidlevere@gmail.com>
2025-09-18 23:45:06 -0400

    bet todo

---
+++ src/compiling/bet.rs
@@ -0,0 +1,11 @@
+
+use super::alef::{AlefFun, AlefStmt, AlefVal};
+
+pub enum BetError {
+
+}
+
+// TODO unwrap nested fun calls into lets
+pub fn compile() -> Result<Vec<AlefFun>, BetError> {
+
+} 

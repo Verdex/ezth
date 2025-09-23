78169ab50c60727e8595dec8d702fdb8ad7984b7
David L. Inman <davidlevere@gmail.com>
2025-09-22 22:51:34 -0400

    compile

--- src/compiling/compiler.rs
+++ src/compiling/compiler.rs
@@ -1,4 +1,6 @@
 
+use std::collections::HashMap;
+use std::rc::Rc;
 use an_a_vm::data::*;
 use crate::data::runtime::*;
 use crate::data::parse::*;
@@ -6,11 +8,12 @@ use super::alef;
 use super::bet::{self, BetExpr, BetFun, BetStmt};
 use super::gimel;
 
-pub fn compile(input : Vec<Def>) -> Result<Vec<Fun<Data>>, Box<dyn std::error::Error>> {
-
-    
-
-    todo!()
+pub fn compile(input : Vec<Def>, ops: &HashMap<Rc<str>, usize>) -> Result<Vec<Fun<Data>>, Box<dyn std::error::Error>> {
+    let input = input.into_iter().map(convert_def).collect::<Vec<_>>();
+    let input = bet::compile(input)?;
+    let input = gimel::compile(input, ops)?;
+    let input = alef::compile(input)?;
+    Ok(input)
 }
 
 fn convert_def(input : Def) -> BetFun {

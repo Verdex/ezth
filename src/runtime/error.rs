6d1e29211377cba3d7f7ec8851f1785fa57c0884
David L. Inman <davidlevere@gmail.com>
2025-09-25 22:26:59 -0400

    runtime error def

---
+++ src/runtime/error.rs
@@ -0,0 +1,15 @@
+
+#[derive(Debug)]
+pub enum RuntimeError {
+    Type(Rc<str>),
+}
+
+impl std::fmt::Display for RuntimeError {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> Result<(), std::fmt::Error> { 
+        match self {
+            _ => todo!(),
+        }
+    }
+}
+
+impl std::error::Error for RuntimeError { }

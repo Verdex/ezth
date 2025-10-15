08237ef47a11d2c82818aff2a609e4527f9cd08b
David L. Inman <davidlevere@gmail.com>
2025-10-14 23:17:18 -0400

    inc prog

--- src/runtime/ops.rs
+++ src/runtime/ops.rs
@@ -8,6 +8,8 @@ pub fn op_list() -> Vec<GenOp<Local, Global>> {
     vec![ 
         GenOp::Local { name: "add".into(), op: add },
         GenOp::DynGlobal { name: cons_op.into(), op: cons },
+        // TODO data_access 
+        GenOp::DynGlobal { name: "data_access".into(), op: data_access },
     ]
 }
 
@@ -39,6 +41,31 @@ fn cons(globals: &mut Vec<Global>, locals: &[Local], params : &[usize]) -> Resul
     }
 }
 
+fn data_access(globals: &mut Vec<Global>, locals: &[Local], params: &[usize]) -> Result<Option<Local>, Box<dyn std::error::Error>> {
+    // params = ref of the data, index to get 
+    // TODO checks
+    //
+    // Note:  This basically works because while creating a Data the 'fields' always get populated
+    // with Ref or Number
+
+    if let Local::Ref(addr) = &locals[params[0]] && 
+       let Local::Number(index) = &locals[params[1]] {
+        
+        // TODO grab the global, it had better either be data or a ref to data
+        // does it have an index that looks good?
+        // that had better be a number or ref
+        // return that
+        todo!()
+    }
+    else {
+        // TODO what about number mismatch?
+        return Err(Box::new(RuntimeError::Type { src: "data_access", expected: "Local::Ref" }));
+    }
+}
+
+// TODO do a set_branch_if_kind_eq or something.  Maybe just a general purpose eq op
+
+
 fn local_to_global(local : &Local) -> Global {
     match local {
         Local::Number(x) => Global::Number(*x),
@@ -46,3 +73,4 @@ fn local_to_global(local : &Local) -> Global {
         Local::Symbol(s) => Global::Data(Rc::clone(s), vec![]),
     }
 }
+
